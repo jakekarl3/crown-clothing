966ce6e5576778f66b546a4b6e2d373eef76a32c
Jake Karl <31318360+jakekarl3@users.noreply.github.com>
2020-05-16 17:48:37 -0500

    Created collectionPreview and collectionOverview components as well as the directory and shop in redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,11 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collectionPreview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-class Shop extends React.Component {
-  constructor() {
-    super();
+const ShopPage = ({ collections }) => (
+  <div className="shop-page">
+    <CollectionsOverview />
+  </div>
+);
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const { collections } = this.state;
-
-    return (
-      <div className="shop-page">
-        {
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    )
-  }
-}
-
-export default Shop;
+export default ShopPage;
